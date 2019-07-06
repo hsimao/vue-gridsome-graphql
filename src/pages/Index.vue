2c2e8b909a6fc681ce09137440ba662dd98ba90b
hsimao <e087754958@gmail.com>
2019-07-06 17:42:28 +0800

    Edit: 修改文字

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,7 +2,7 @@
     <Layout :showFooter="true">
         <!-- Learn how to use images here: https://gridsome.org/docs/images -->
         <g-image alt="Example image" src="~/assets/images/favicon.png" width="135" />
-        <h1>kkHello, world!</h1>
+        <h1>Hello, world!</h1>
 
         <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
